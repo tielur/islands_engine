37ea7768c742748b248a87c17fb2cc9360aeb0a6
Tyler Clemens <tyler.r.clemens@gmail.com>
2019-03-02 16:00:12 -0700

    Add add/3 to Guesses

--- test/guesses_test.exs
+++ test/guesses_test.exs
@@ -2,11 +2,39 @@ defmodule IslandsEngine.GuessesTest do
   use ExUnit.Case
   doctest IslandsEngine.Guesses
 
-  alias IslandsEngine.Guesses
+  alias IslandsEngine.{
+    Coordinate,
+    Guesses
+  }
 
   describe "new/0" do
     test "returns a Guesses struct with empty MapSets" do
       assert %Guesses{hits: %MapSet{}, misses: %MapSet{}} = Guesses.new()
+    end
+  end
+
+  describe "add/3" do
+    setup do
+      {:ok, guesses: Guesses.new()}
+    end
+
+    test "adds to misses when given :miss", %{guesses: guesses} do
+      {:ok, coordinate} = Coordinate.new(1, 2)
+      guesses = Guesses.add(guesses, :miss, coordinate)
+      assert MapSet.member?(guesses.misses, coordinate)
+    end
+
+    test "adds to hits when given :hit", %{guesses: guesses} do
+      {:ok, coordinate} = Coordinate.new(1, 2)
+      guesses = Guesses.add(guesses, :hit, coordinate)
+      assert MapSet.member?(guesses.hits, coordinate)
+    end
+
+    test "raises when given anything else", %{guesses: guesses} do
+      {:ok, coordinate} = Coordinate.new(1, 2)
+      assert_raise FunctionClauseError, fn ->
+        Guesses.add(guesses, :something_else, coordinate)
+      end
     end
   end
 end
