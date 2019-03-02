ef4f0c40cb28e5748fa5aff5cee39470df7175a6
Tyler Clemens <tyler.r.clemens@gmail.com>
2019-03-02 15:34:37 -0700

    Wrap Island new/2 tests in describe

--- test/island_test.exs
+++ test/island_test.exs
@@ -7,18 +7,20 @@ defmodule IslandsEngine.IslandTest do
     Island
   }
 
-  test "when given a correct type and coordinate it returns {:ok, %Island{}}" do
-    {:ok, coordinate} = Coordinate.new(4, 6)
-    assert {:ok, %Island{}} = Island.new(:l_shape, coordinate)
-  end
+  describe "new/2" do
+    test "when given a correct type and coordinate it returns {:ok, %Island{}}" do
+      {:ok, coordinate} = Coordinate.new(4, 6)
+      assert {:ok, %Island{}} = Island.new(:l_shape, coordinate)
+    end
 
-  test "when given a wrong type it returns an error" do
-    {:ok, coordinate} = Coordinate.new(1, 1)
-    assert {:error, :invalid_island_type} = Island.new(:wrong_type, coordinate)
-  end
+    test "when given a wrong type it returns an error" do
+      {:ok, coordinate} = Coordinate.new(1, 1)
+      assert {:error, :invalid_island_type} = Island.new(:wrong_type, coordinate)
+    end
 
-  test "when given an invalid coordinate it returns an error" do
-    {:ok, coordinate} = Coordinate.new(10, 10)
-    assert {:error, :invalid_coordinate} = Island.new(:l_shape, coordinate)
+    test "when given an invalid coordinate it returns an error" do
+      {:ok, coordinate} = Coordinate.new(10, 10)
+      assert {:error, :invalid_coordinate} = Island.new(:l_shape, coordinate)
+    end
   end
 end
