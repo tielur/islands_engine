16fbde47a6680840493d8f8de0740001b34865c6
Tyler Clemens <tyler.r.clemens@gmail.com>
2019-03-02 16:14:55 -0700

    Add overlaps?/2 to Island

--- lib/islands_engine/island.ex
+++ lib/islands_engine/island.ex
@@ -16,6 +16,10 @@ defmodule IslandsEngine.Island do
     end
   end
 
+  def overlaps?(existing_island, new_island) do
+    not MapSet.disjoint?(existing_island.coordinates, new_island.coordinates)
+  end
+
   defp offsets(:square), do: [{0, 0}, {0, 1}, {1, 0}, {1, 1}]
   defp offsets(:atoll), do: [{0, 0}, {0, 1}, {1, 1}, {2, 0}, {2, 1}]
   defp offsets(:dot), do: [{0, 0}]
--- test/island_test.exs
+++ test/island_test.exs
@@ -23,4 +23,26 @@ defmodule IslandsEngine.IslandTest do
       assert {:error, :invalid_coordinate} = Island.new(:l_shape, coordinate)
     end
   end
+
+  describe "overlaps?/2" do
+    test "when two shapes overlap it returns true" do
+      {:ok, square_coordinate} = Coordinate.new(1, 1)
+      {:ok, square} = Island.new(:square, square_coordinate)
+
+      {:ok, dot_coordinate} = Coordinate.new(1, 2)
+      {:ok, dot} = Island.new(:dot, dot_coordinate)
+
+      assert true = Island.overlaps?(square, dot)
+    end
+
+    test "when two shapes don't overlap it returns false" do
+      {:ok, square_coordinate} = Coordinate.new(1, 1)
+      {:ok, square} = Island.new(:square, square_coordinate)
+
+      {:ok, l_shape_coordinate} = Coordinate.new(5, 5)
+      {:ok, l_shape} = Island.new(:l_shape, l_shape_coordinate)
+
+      assert false == Island.overlaps?(square, l_shape)
+    end
+  end
 end
