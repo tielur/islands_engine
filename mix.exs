e491ba2530d63a0ece33bd4f6e1e1f9409d625aa
Tyler Clemens <tyler.r.clemens@gmail.com>
2019-03-02 17:59:03 -0700

    Add coveralls

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule IslandsEngine.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -23,7 +30,9 @@ defmodule IslandsEngine.MixProject do
   defp deps do
     [
       {:credo, "~> 1.0", runtime: false, only: [:dev, :test]},
-      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev, :test], runtime: false}
+      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test}
+
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
